6c6a4a3653188ac08c550bc43b84c6c1bba01e9c
Tom Summersell <tsummersell@hotmail.com>
2023-07-30 16:30:35 +0100

    Loading config file and basic usage

--- bot.py
+++ bot.py
@@ -5,6 +5,10 @@ import sys
 import yaml
 import discord.ext.commands
 
+# Load our config file
+with open('config.yaml', 'r') as file:
+    config = yaml.safe_load(file)
+
 # Set up a logger so we can send just the card to stdout
 handler = logging.StreamHandler()
 
@@ -39,7 +43,18 @@ async def on_ready():
     discord_logger.info(f'We have logged in as {bot.user}')
 
 @bot.command()
-async def bingo(ctx):
-    await ctx.reply('Let\'s play Bingo!', mention_author=True)
+async def bingo(ctx, arg='help'):
+    if arg == 'list':
+        reply = []
+        reply.append("Available games:")
+        for game in config['sets']:
+            reply.append(f'- `{game}`: {config["sets"][game]["description"]}')
+            await ctx.reply('\n'.join(reply), mention_author=True)
+    elif arg == 'help':
+        await ctx.reply('Usage: Type `!bingo game` to play a specific game, `!bingo list` to list games, or `"bingo help` for this message')
+    else:
+        pass
+
+
 
 bot.run(os.environ['DISCORD_TOKEN'], log_handler=None)
